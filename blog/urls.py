77fe64b5e812623310690f08e6194c898d3e23dd
Anton Byshok <anton.byshok@gmail.com>
2024-11-20 15:29:12 +0000

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     # Django is looking for a URL that matches blog/ and then it will execute the PostList view from our views.py file.
     path('blog/', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
