f5ea89e798dde73b042e0a4b32627983c76cc605
Anton Byshok <anton.byshok@gmail.com>
2024-11-21 00:04:42 +0000

    function-based view for a single blog post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,12 @@ from django.urls import path
 urlpatterns = [
     # Django is looking for a URL that matches blog/ and then it will execute the PostList view from our views.py file.
     path('blog/', views.PostList.as_view(), name='home'),
+    # <slug:slug> is where the slug value is passed from the template's URL tag.
+    # This urlpattern (<slug:slug>) creates a url path of the domain path plus the slug value.
+    #   The slug path converter before the colon defines the data type as a slug, 
+    # and the slug after the colon is the post.slug value passed from the template. 
+    # You see this value in the URL path in the browser bar.
+    #   If you had a human resources web app that identified workers by 
+    # their ID badge number, then you could use the syntax <int:id_badge>
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-        # Django's generic views
+
+# Django's generic views
 class PostList(generic.ListView):
     # creates queryset (equal to  queryset = Post.objects.all())
     # model = Post     # returned all of the records from our Post model (all of the blog posts).   
@@ -15,25 +16,15 @@ class PostList(generic.ListView):
     paginate_by = 6    
 
 
+# this is a function-based view, no as_view() method is required. 
+# A view function takes a web request and returns a web response.
+# The slug parameter gets the argument value from the URL pattern named post_detail. (the slug value is unique)
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
+    # The response our view is returning is the contents of a webpage containing one post.
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post}, # a dictionary of data
     )    
