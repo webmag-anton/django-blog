d264022e447acd2095f0ab09f904fdbb51079754
Anton Byshok <anton.byshok@gmail.com>
2024-11-25 15:31:20 +0000

    tyding up

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
-from .models import Comment
 from django import forms
+from .models import Comment
 
 # CommentForm inherites from Django's forms.ModelForm class
 class CommentForm(forms.ModelForm):
